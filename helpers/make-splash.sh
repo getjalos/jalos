ea4172488eef8e60f2149de35051b6b4dfc557c2
Julio Jimenez <julio@julioj.com>
2026-01-22 20:35:36 -0500

    move it up
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- helpers/make-splash.sh
+++ helpers/make-splash.sh
@@ -20,24 +20,24 @@ echo "Creating splash images..."
 
 # GRUB splash (800x600)
 convert -size 800x600 xc:'#000' \
-  \( logo.png -resize 400x400 \) -gravity West -geometry +0+0 -composite \
+  \( logo.png -resize 400x400 \) -gravity West -geometry +0-100 -composite \
   -font DejaVu-Sans-Bold -pointsize 28 -fill white \
-  -gravity West -annotate +400-40 "jalos 26.01.00 amd64" \
+  -gravity West -annotate +400-140 "jalos 26.01.00 amd64" \
   -font DejaVu-Sans -pointsize 16 -fill white \
-  -annotate +400+0 "Built $(date -u +'%Y-%m-%d %H:%M:%S UTC')" \
+  -annotate +400-100 "Built $(date -u +'%Y-%m-%d %H:%M:%S UTC')" \
   -pointsize 12 -fill '#a0a0a0' \
-  -annotate +400+60 "linux: 6.12.63-1\nlive-build: 20250505+deb13u1\nlive-boot: 1:20250815~deb13u1\nlive-config: 11.0.5\nlive-tools: (none)" \
+  -annotate +400-40 "linux: 6.12.63-1\nlive-build: 20250505+deb13u1\nlive-boot: 1:20250815~deb13u1\nlive-config: 11.0.5\nlive-tools: (none)" \
   ../live/config/bootloaders/grub-pc/splash.png
 
 # SYSLINUX splash (640x480)
 convert -size 640x480 xc:'#000' \
-  \( logo.png -resize 320x320 \) -gravity West -geometry +0+0 -composite \
+  \( logo.png -resize 320x320 \) -gravity West -geometry +0-80 -composite \
   -font DejaVu-Sans-Bold -pointsize 24 -fill white \
-  -gravity West -annotate +320-40 "jalos 26.01.00 amd64" \
+  -gravity West -annotate +320-120 "jalos 26.01.00 amd64" \
   -font DejaVu-Sans -pointsize 14 -fill white \
-  -annotate +320+0 "Built $(date -u +'%Y-%m-%d %H:%M:%S UTC')" \
+  -annotate +320-80 "Built $(date -u +'%Y-%m-%d %H:%M:%S UTC')" \
   -pointsize 11 -fill '#a0a0a0' \
-  -annotate +320+60 "linux: 6.12.63-1\nlive-build: 20250505+deb13u1\nlive-boot: 1:20250815~deb13u1\nlive-config: 11.0.5\nlive-tools: (none)" \
+  -annotate +320-20 "linux: 6.12.63-1\nlive-build: 20250505+deb13u1\nlive-boot: 1:20250815~deb13u1\nlive-config: 11.0.5\nlive-tools: (none)" \
   ../live/config/bootloaders/syslinux/splash.png
 
 echo "Splash images created successfully."
